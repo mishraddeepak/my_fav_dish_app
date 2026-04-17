651bb8b13090a38a7df4812af8ccb76dec6071a8
Deepak Mishra <143652878+mishraddeepak@users.noreply.github.com>
2026-04-17 18:37:48 +0530

    Update app1.js

--- app1.js
+++ app1.js
@@ -48,6 +48,7 @@ function removeItemFromLocalStorage(itemToRemove) {
 function fetchResult(query) {
     console.log("nsjndjnjdjn")
     console.log("Hiii.....rupali")
+    console.log("hii....amit")
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
         .then((response) => response.json())
         .then((data) => {
